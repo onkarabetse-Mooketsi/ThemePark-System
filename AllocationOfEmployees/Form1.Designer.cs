c5f2f0cb4253ebff41530c68c6eb369a95139ece
onkarabetse-Mooketsi <mooketsionkarabetse4@gmail.com>
2023-08-30 20:01:17 +0200

    My Allocation form

---
+++ AllocationOfEmployees/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace AllocationOfEmployees
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.gpbAllocations = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dgvAllocations = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnAllocate = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnMainMenu = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.gpbAllocations.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAllocations)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::AllocationOfEmployees.Properties.Resources.themePark_emp;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(295, 190);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // gpbAllocations
+            // 
+            this.gpbAllocations.Controls.Add(this.btnMainMenu);
+            this.gpbAllocations.Controls.Add(this.btnDelete);
+            this.gpbAllocations.Controls.Add(this.btnUpdate);
+            this.gpbAllocations.Controls.Add(this.btnAllocate);
+            this.gpbAllocations.Controls.Add(this.txtSearch);
+            this.gpbAllocations.Controls.Add(this.label2);
+            this.gpbAllocations.Controls.Add(this.dgvAllocations);
+            this.gpbAllocations.Location = new System.Drawing.Point(12, 199);
+            this.gpbAllocations.Name = "gpbAllocations";
+            this.gpbAllocations.Size = new System.Drawing.Size(738, 259);
+            this.gpbAllocations.TabIndex = 1;
+            this.gpbAllocations.TabStop = false;
+            this.gpbAllocations.Text = "Allocations";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(313, 83);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(233, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Allocation of employees";
+            // 
+            // dgvAllocations
+            // 
+            this.dgvAllocations.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvAllocations.Location = new System.Drawing.Point(7, 20);
+            this.dgvAllocations.Name = "dgvAllocations";
+            this.dgvAllocations.Size = new System.Drawing.Size(591, 198);
+            this.dgvAllocations.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 228);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Search:";
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(58, 225);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(237, 20);
+            this.txtSearch.TabIndex = 2;
+            // 
+            // btnAllocate
+            // 
+            this.btnAllocate.Location = new System.Drawing.Point(613, 20);
+            this.btnAllocate.Name = "btnAllocate";
+            this.btnAllocate.Size = new System.Drawing.Size(115, 29);
+            this.btnAllocate.TabIndex = 3;
+            this.btnAllocate.Text = "Allocate";
+            this.btnAllocate.UseVisualStyleBackColor = true;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(613, 80);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(115, 29);
+            this.btnUpdate.TabIndex = 4;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(613, 135);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(115, 29);
+            this.btnDelete.TabIndex = 5;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnMainMenu
+            // 
+            this.btnMainMenu.Location = new System.Drawing.Point(613, 189);
+            this.btnMainMenu.Name = "btnMainMenu";
+            this.btnMainMenu.Size = new System.Drawing.Size(115, 29);
+            this.btnMainMenu.TabIndex = 6;
+            this.btnMainMenu.Text = "Main Menu";
+            this.btnMainMenu.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(761, 463);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.gpbAllocations);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Allocation of employees";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.gpbAllocations.ResumeLayout(false);
+            this.gpbAllocations.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAllocations)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox gpbAllocations;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnAllocate;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dgvAllocations;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnMainMenu;
+    }
+}
+
